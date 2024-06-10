5ef491b4563184c4773dd4cd61a3f64471c89593
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-06-10 18:11:22 -0400

    Add initial endpoints and handlebars templates

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,7 @@ const { Category, Item } = require("../models");
 
 router.use("/api", apiRoutes);
 
+// Home page
 router.get("/", async (req, res) => {
   try {
     const categories = await Category.getCategories();
@@ -19,11 +20,45 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Category page
 router.get("/categories", async (req, res) => {
   try {
     const categories = await Category.getCategories();
 
     res.render("categories", { categories });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Dashboard page
+router.get("/dashboard", async (req, res) => {
+  try {
+    const data = {};
+    res.render("dashboard", { data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+//login page
+router.get("/login", async (req, res) => {
+  try {
+    const data = {};
+    res.render("login", { data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+//signup page
+router.get("/signup", async (req, res) => {
+  try {
+    const data = {};
+    res.render("signup", { data });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
