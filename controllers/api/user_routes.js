a66e39b6b5a62f2db9d553f31d258c7cefdb75e6
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-06-11 12:58:58 -0400

    fixed minor errors in login and signup post routes

--- controllers/api/user_routes.js
+++ controllers/api/user_routes.js
@@ -10,9 +10,9 @@ router.post('/signup', async (req, res) => {
         const { username, password, phone_email, address } = req.body
 
         //check if user exists by searching for username
-        const user = await User.findOne({ where: { username } })
+        const existingUser = await User.findOne({ where: { username } })
         //if username exists, render message below
-        if (user) {
+        if (existingUser) {
             res.status(400).json({ message: 'Username already exists' })
         }
 
@@ -20,14 +20,14 @@ router.post('/signup', async (req, res) => {
         const hashPassword = await bcrypt.hash(password, 10)
 
         //create new user
-        const newUser = User.create({
+        const newUser = await User.create({
             username,
             password: hashPassword,
             phone_email,
             address
         })
         //redirect to homepage once signup is successful
-        res.redirect('/')
+        res.status(200).json({ message: 'New user created' })
     } catch (error) {
         console.error(error)
         res.status(400).json(error)
@@ -41,15 +41,16 @@ router.post('/login', async (req, res) => {
         //get username, password from req body
         const { username, password } = req.body
 
-        const user = User.findAll(user => user.username === username)
+        // const user = await User.findOne(user => user.username === username)
+        const currentUser = await User.findOne({ where: { username } })
 
-        if (!user) {
+        if (!currentUser) {
             return res.status(404).json({ message: 'invalid username or password' })
         }
 
-        const checkPassword = await bcrypt.compare(password, user.password)
+        const checkPassword = await bcrypt.compare(password, currentUser.password)
         if (!checkPassword) {
-            return res.status(404).json({ message: 'invalid username or password' })
+            return res.status(401).json({ message: 'invalid username or password' })
         }
     } catch (error) {
         console.error(error)
