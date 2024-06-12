a20f2e6e949502bd9f9e938e136bc1163713b10f
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-06-12 16:24:46 -0400

    Make sure the location is set before running the serach

--- public/js/category_scroll_box.js
+++ public/js/category_scroll_box.js
@@ -11,9 +11,27 @@ function findParentWithClass(element, className) {
 }
 
 function categoryClicked(e) {
+  const coords = JSON.parse(localStorage.getItem("coords"));
+  if (!coords) {
+    alert("Please enter a location to search for items");
+    return;
+  }
+  const { lat, lng } = JSON.parse(localStorage.getItem("coords"));
+  // Get the category name
   const element = findParentWithClass(e.target, "category-item");
   const category = element.getAttribute("data-category-name");
-  window.location.href = `api/items/search/category/${category}`;
+
+  //  Get the radius in meters
+  let radius = document.querySelector("#search-radius").value.trim();
+  radius = parseFloat(radius) * 1000;
+
+  let url = new URL("/api/items/search/category", window.location.origin);
+  url.searchParams.append("lat", lat);
+  url.searchParams.append("lng", lng);
+  url.searchParams.append("category", category);
+  url.searchParams.append("radius", radius);
+
+  window.location.href = url;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
