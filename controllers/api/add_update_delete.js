8b82e4e922acd9501250c8fe44e5a3888cfad7be
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-06-12 19:56:35 -0400

    added starter code for post, put, delete routes

--- controllers/api/add_update_delete.js
+++ controllers/api/add_update_delete.js
@@ -7,3 +7,18 @@ const upload = multer({
     storage: storage,
     limits: { files: 3 }
 })
+
+//add items
+router.post('/new/items', async (req, res) => {
+    //logic for adding items
+})
+
+//update items
+router.put('/new/items/:id', async (req, res) => {
+    //logic for updating items
+})
+
+//delete items
+router.delete('/new/items/:id', (req, res) => {
+    //logic for deleting items
+})
