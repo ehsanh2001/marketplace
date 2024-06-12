daeef6d86cd08304ed99e7541912eefd599250d1
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-06-12 18:45:14 -0400

    Solve conflicts

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,11 @@
 
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const homeRoutes = require("./home_routes");
 const { Category, Item } = require("../models");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-
 
 router.use((req, res, next) => {
   res.status(404).send("Sorry can't find the page you are looking for!");
