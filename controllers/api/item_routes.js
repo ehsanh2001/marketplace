312a06be44f055c0814b59db2e116d865d94c49e
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-06-12 16:23:31 -0400

    Refactoring the search for items

--- controllers/api/item_routes.js
+++ controllers/api/item_routes.js
@@ -2,13 +2,16 @@
 
 const router = require("express").Router();
 const { Category, Item } = require("../../models");
-const { searchItems, searchItemsByCategory } = require("../../models/queries");
+const {
+  searchItemsByTerm,
+  searchItemsByCategory,
+} = require("../../models/queries");
 
 // search based on term and location
-// query: /api/item/term_location?term=term&location=location&radius=radius
+// query: /api/item/term_location?term=term&lat=lat&lng=lng&radius=radius
 router.get("/search/term_location", async (req, res) => {
   try {
-    const data = await searchItems(req.query);
+    const data = await searchItemsByTerm(req.query);
     console.dir(data);
     res.render("search_result", {
       data,
@@ -22,9 +25,10 @@ router.get("/search/term_location", async (req, res) => {
 });
 
 // search based on category
-router.get("/search/category/:name", async (req, res) => {
+// query: /api/item/search/category?name=category&lat=lat&lng=lng&radius=radius
+router.get("/search/category/", async (req, res) => {
   try {
-    const data = await searchItemsByCategory(req.params.name);
+    const data = await searchItemsByCategory(req.query);
     res.render("search_result", { data, category: req.params.name });
   } catch (err) {
     console.error(err);
--- models/queries.js
+++ models/queries.js
@@ -18,21 +18,21 @@ async function getNFreeItems(n) {
   }
 }
 
-// search items based on term and location
-async function searchItems(searchParams) {
-  const { term, lat, lng, radius } = searchParams;
+// search items based on location and added where clause
+async function searchItems(searchParams, addedWhereClause) {
+  const { lat, lng, radius } = searchParams;
   try {
     // get items based on term and location
     const result = await sequelize.query(
       `
-        select distinct item.id , title,description,phone_email,price,item.username,address, image.id as image_id,ST_Distance(
+        select distinct cat_name, item.id , title,description,phone_email,price,item.username,address, image.id as image_id,ST_Distance(
            geo_location, 
            ST_SetSRID(ST_MakePoint(${lng}, ${lat}), 4326)
          )  AS distance
         from item inner join users on item.username = users.username
             inner join image on item.id = image.item_id
   
-        where 	title ilike '%${term}%' and
+        where 	${addedWhereClause} and
               ST_Distance(
            geo_location, 
            ST_SetSRID(ST_MakePoint(${lng}, ${lat}), 4326)
@@ -69,46 +69,23 @@ async function searchItems(searchParams) {
   }
 }
 
+async function searchItemsByTerm(searchParams) {
+  const { term } = searchParams;
+  return await searchItems(searchParams, `title ilike '%${term}%'`);
+}
+
 // search items based on category
-async function searchItemsByCategory(categoryName) {
+async function searchItemsByCategory(searchParams) {
+  const { category, lat, lng, radius } = searchParams;
+  return await searchItems(searchParams, `cat_name = '${category}'`);
+}
+
+// search items based on item id
+async function searchItemsById(searchParams) {
   try {
-    // replace `'` with `''` to escape the single quote
-    const catName = categoryName.replace(/'/g, "''");
-    // get items based on category
-    const result = await sequelize.query(
-      `
-        select distinct cat_name,item.id , title,description,phone_email,price,item.username,address, image.id as image_id
-        from item inner join users on item.username = users.username
-            inner join image on item.id = image.item_id
-        where cat_name = '${catName}'
-        `,
-      { type: sequelize.QueryTypes.SELECT }
-    );
-
-    // reduce the result to group by item, so each item has only one entry and an array of images
-    const groupedResult = result.reduce((acc, curr) => {
-      const found = acc.find((item) => item.id === curr.id);
-      if (!found) {
-        acc.push({
-          cat_name: curr.cat_name,
-          id: curr.id,
-          title: curr.title,
-          description: curr.description,
-          phone_email: curr.phone_email,
-          price: curr.price,
-          username: curr.username,
-          address: curr.address,
-          images: [{ id: curr.image_id }],
-        });
-      } else {
-        found.images.push({ id: curr.image_id });
-      }
-      return acc;
-    }, []);
-
-    return groupedResult;
   } catch (err) {
     console.error(err);
   }
 }
-module.exports = { getNFreeItems, searchItems, searchItemsByCategory };
+
+module.exports = { getNFreeItems, searchItemsByCategory, searchItemsByTerm };
