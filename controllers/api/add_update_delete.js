fdc9adf4293beab9cf75857e0db0b2d5b16ab1fa
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-06-13 12:57:01 -0400

    Reformatting the code

--- controllers/api/add_update_delete.js
+++ controllers/api/add_update_delete.js
@@ -1,87 +1,86 @@
-const router = require('express').Router()
-const { item, image } = require("../../models")
-const multer = require('multer')
+const router = require("express").Router();
+const { item, image } = require("../../models");
+const multer = require("multer");
 
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage();
 const upload = multer({
-    storage: storage,
-    limits: { files: 3 }
-})
+  storage: storage,
+  limits: { files: 3 },
+});
 
 //add items
-router.post('/new/items', upload.array('images', 3), async (req, res) => {
-    try {
-        const newItem = await item.create(req.body)
+router.post("/new/items", upload.array("images", 3), async (req, res) => {
+  try {
+    const newItem = await item.create(req.body);
 
-        if (req.files) {
-            for (const newFile of req.files) {
-                await image.create({
-                    item_id: newItem.id,
-                    image: newFile.buffer
-                })
-            }
-        }
-
-        res.status(200).json(newItem)
-    } catch (error) {
-        console.error(error)
-        res.status(400).json(error)
+    if (req.files) {
+      for (const newFile of req.files) {
+        await image.create({
+          item_id: newItem.id,
+          image: newFile.buffer,
+        });
+      }
     }
-})
+
+    res.status(200).json(newItem);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
 
 //update items
-router.put('/new/items/:id', upload.array('images', 3), async (req, res) => {
-    //logic for updating items
-    try {
-        const updateItems = await item.update(req.body, {
-            where: {
-                id: req.params.id
-            }
-        })
+router.put("/new/items/:id", upload.array("images", 3), async (req, res) => {
+  //logic for updating items
+  try {
+    const updateItems = await item.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
 
-        if (req.files) {
-            await image.destroy({
-                where: {
-                    item_id: req.params.id
-                }
-            })
+    if (req.files) {
+      await image.destroy({
+        where: {
+          item_id: req.params.id,
+        },
+      });
 
-            for (const newFile of req.files) {
-                await image.create({
-                    item_id: req.params.id,
-                    image: newFile.buffer
-                })
-            }
-        }
-
-        res.status(200).json(updateItems)
-
-    } catch (error) {
-        console.error(error)
-        res.status(400).json(error)
+      for (const newFile of req.files) {
+        await image.create({
+          item_id: req.params.id,
+          image: newFile.buffer,
+        });
+      }
     }
-})
+
+    res.status(200).json(updateItems);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
 
 //delete items
-router.delete('/new/items/:id', async (req, res) => {
-    try {
-        const deletedItem = await item.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
+router.delete("/new/items/:id", async (req, res) => {
+  try {
+    const deletedItem = await item.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-        await image.destroy({
-            where: {
-                item_id: req.params.id
-            }
-        })
+    await image.destroy({
+      where: {
+        item_id: req.params.id,
+      },
+    });
 
-        res.status(200).json(deletedItem)
-    } catch (error) {
-        console.error(error)
-        res.status(400).json(error)
-    }
-})
+    res.status(200).json(deletedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
